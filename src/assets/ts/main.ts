6812edcb60f5eb10556c8985f826c509c3ccef50
realmaxv <max.vossgaetter@gmx.de>
2025-03-21 09:01:48 +0100

    new tailwindcss

--- src/assets/ts/main.ts
+++ src/assets/ts/main.ts
@@ -89,3 +89,27 @@ calculateBtn.addEventListener("click", () => {
 
   console.log(totalCalories);
 });
+
+const intersectionObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("in-view");
+        entry.target.classList.remove("not-in-view");
+      } else {
+        entry.target.classList.remove("in-view");
+        entry.target.classList.add("not-in-view");
+      }
+    });
+  },
+  {
+    rootMargin: "0px",
+    threshold: [0, 0.1, 1],
+  }
+);
+
+const tags = document.querySelectorAll(".left, .right");
+
+tags.forEach((tag) => {
+  intersectionObserver.observe(tag);
+});
